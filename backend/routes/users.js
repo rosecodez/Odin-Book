2a881927e1bc974e645cb607570cef3b22a89d1c
rosecodez <rosebeats09@gmail.com>
2024-10-18 19:32:49 -0400

    Set up update profile picture route

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("..controllers./userController");
+const upload = require("../middleware/multer");
 
 router.post("/sign-up", userController.user_signup_post);
 
@@ -8,5 +9,11 @@ router.get("/log-in", userController.user_login_get);
 router.post("/log-in", userController.user_login_post);
 
 router.post("/log-out", userController.user_logout_post);
+
+router.post(
+  "/update-profile-picture",
+  upload.single("file"),
+  userController.user_update_profile_picture
+);
 
 module.exports = router;
