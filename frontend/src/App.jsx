30dc72204017c9a64fd5ff7493843b94c5d60a5a
rosecodez <rosebeats09@gmail.com>
2024-11-02 13:16:11 -0400

    Always display header and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Home from './pages/homePage';
+import Header from './components/header';
+import Footer from './components/footer';
 
 function App() {
   return (
     <Router>
+      <Header/>
       <Routes>
         <Route path="/" element={<Home/>}/>
       </Routes>
+      <Footer/>
     </Router>
   )
 }
