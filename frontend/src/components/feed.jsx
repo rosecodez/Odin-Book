f5aa89d35c054b8f3e1a31e710939f9f6fe46306
rosecodez <rosebeats09@gmail.com>
2024-12-14 13:26:03 -0500

    Arrange feed page code

--- frontend/src/components/feed.jsx
+++ frontend/src/components/feed.jsx
@@ -152,50 +152,69 @@ export default function Feed({ isAuthenticated, isVisitor, setIsVisitor }) {
             const formattedDate = DateTime.fromISO(post.created_at).toLocaleString({ month: 'short', day: '2-digit' });
             
             return (
-              <a href={`/posts/${post.id}`}>
-                <li key={post.id} className="flex flex-col">
+              <div>
 
-                  <div className="flex flex-row gap-[14px] w-full">
-                    <img src={post.user?.profile_image || camera} className="rounded-full w-[50px] h-[50px] pt-[15px]" alt="Profile" />
-                    
-                    <div className="flex gap-2 mt-[7px] w-full justify-start">
-                      <a href={`/users/${post.user.username}`}>{post.user.username}</a>
-                      <p>{formattedDate}</p>
-                    </div>
+                <a href={`/posts/${post.id}`}>
 
-                  </div>
+                  <li key={post.id}>
 
-                  <div className="flex flex-col gap-2 pl-16">
+                    <div className="flex flex-row gap-[19px] w-full">
 
-                    {editPostId === post.id ? (
-                      <div>
-                        <textarea value={editedContent} onChange={(e) => setEditedContent(e.target.value)} className="w-full p-2 border rounded"/>
-                        <div className="flex gap-2 mt-2">
-                          <button onClick={() => {saveEdit(post.id), setIsEditing(false);}}className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-4 rounded">Save</button>
-                          <button onClick={cancelEdit} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-4 rounded">Cancel</button>
+                      <a href={`/users/${post.user.username}`}>
+                        <img src={post.user.profile_image} className="rounded-full w-[50px] h-[50px]"/>
+                      </a>
+
+                      <div className="flex gap-2 mt-[7px] w-full justify-between">
+                        <div className="flex gap-2">
+                          <a href={`/users/${post.user.username}`}>
+                              {post.user.username}
+                          </a>
+                          <p>{formattedDate}</p>
                         </div>
+
                       </div>
-                      ) : (
-                        <p className="w-full break-words">{post.content}</p>
-                      )
-                    }
-                    {post.post_image && <img src={post.post_image} alt="post image" />}
-                  </div>
 
-                  <div className="flex flex-row justify-between pl-[64px]">
-                    <div className="flex flex-row gap-2 items-start">
-                      <img src={message} className="w-[25px] h-[25px]" alt="Messages" />
-                      <p>0</p>
                     </div>
 
-                    <div className="flex flex-row gap-2 items-start pr-[3px]">
-                      <img src={heart} className="w-[25px] h-[25px]" alt="Likes" />
-                      <p>0</p>
-                    </div>
-                  </div>
+                    <div className="flex flex-col gap-2 pl-16">
 
-                </li>
-              </a>
+                      {editPostId === post.id ? (
+                        <div>
+                            <textarea value={editedContent} onChange={(e) => setEditedContent(e.target.value)} className="w-full p-2 border rounded"/>
+                            <div className="flex gap-2 mt-2">
+                              <button onClick={() => {saveEdit(post.id), setIsEditing(false);}} className="bg-green-500 hover:bg-green-700 text-white font-bold py-1 px-4 rounded">Save</button>
+                              <button onClick={cancelEdit} className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-1 px-4 rounded">Cancel</button>
+                            </div>
+                        </div>
+                        ) : (
+                          <p className="w-full break-words">{post.content}</p>
+                        )
+                      }
+
+                      {post.post_image && <img src={post.post_image} alt="post image" />}
+
+                    </div>
+
+                    <div className="flex flex-row justify-between pl-[64px]">
+
+                        <div className="flex flex-row gap-2 items-start">
+                            <img src={message} className="w-[25px] h-[25px]" alt="Messages" />
+                            <p>0</p>
+                        </div>
+
+                        <div className="flex flex-row gap-2 items-start pr-[3px]">
+                            <img src={heart} className="w-[25px] h-[25px]" alt="Likes" />
+                            <p>0</p>
+                        </div>
+
+                    </div>
+
+                  </li>
+
+                </a>
+
+              </div>
+              
               
             );
           })
