eb76fe7e353ad06461313b78a6379a03257b35f0
rosecodez <rosebeats09@gmail.com>
2025-02-12 18:40:56 -0500

    Allow cors from render backend site

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://odin-book-frontend.onrender.com',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
