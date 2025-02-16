677202613271b4fefb054b5b717d41df4f9ca0dc
rosecodez <rosebeats09@gmail.com>
2025-02-16 14:12:41 -0500

    Check authenticated logs

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -1,10 +1,10 @@
 function isAuthenticated(req, res, next) {
-  console.log(req.session);
-  console.log(req.user);
-  console.log(req.session.user);
-  console.log(req.user, req.session.user);
+  console.log(req.session + 'req session');
+  console.log(req.user + 'req.user');
+  console.log(req.session.user + 'req.session.user');
 
-  if (req.user || (req.session.user && req.session.user.isVisitor === true)) {
+  if (req.user || (req.session?.user && req.session.user.isVisitor === true)) {
+    console.log('req user is visitor true');
     return next();
   } else {
     return res.status(401).json({ message: 'Unauthorized' });
