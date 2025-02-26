fb855f94433ac961d1f14dc0b4e19ec164c67e75
rosecodez <rosebeats09@gmail.com>
2025-02-25 19:53:41 -0500

    Ensure cors headers are set before middleware

--- backend/middleware/authentication.js
+++ backend/middleware/authentication.js
@@ -2,6 +2,11 @@ function isAuthenticated(req, res, next) {
   console.log(req.session);
   console.log(req.user);
   console.log(req.session?.user);
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://odin-book-frontend.onrender.com'
+  );
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
 
   if (!req.user && !req.session?.user) {
     console.log('unauthorized');
