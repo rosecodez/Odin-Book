7b79e4a4373a614f3f83d59370f5ea7b6f6e3ce2
rosecodez <rosebeats09@gmail.com>
2025-03-07 18:26:25 -0500

    Verify if google auth is called

--- backend/app.js
+++ backend/app.js
@@ -213,4 +213,8 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
+app._router.stack.forEach((middleware) => {
+  console.log(`middleware`, middleware.route.path);
+});
+
 module.exports = app;
