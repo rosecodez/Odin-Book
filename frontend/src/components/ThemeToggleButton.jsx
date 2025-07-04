89b5e1a657cbe9000ceb64e60a6ddfef81e9b288
Roxana Dandu <rosebeats09@gmail.com>
2025-07-03 20:04:04 -0400

    Change theme style

--- frontend/src/components/ThemeToggleButton.jsx
+++ frontend/src/components/ThemeToggleButton.jsx
@@ -1,11 +1,8 @@
 import { useEffect, useState } from "react";
-import DarkThemeBtn from "../assets/icons8-dark-theme-30.png";
-import LightThemeBtn from "../assets/icons8-light-on-50.png";
 
 export default function ThemeToggleButton() {
   const [isDark, setIsDark] = useState(() => {
-    const storedTheme = localStorage.getItem("theme");
-    return storedTheme === "dark";
+    return localStorage.getItem("theme") === "dark";
   });
 
   useEffect(() => {
@@ -15,12 +12,42 @@ export default function ThemeToggleButton() {
   }, [isDark]);
 
   return (
-    <button onClick={() => setIsDark(!isDark)} className="btn btn-ghost btn-sm">
-      <img
-        src={isDark ? LightThemeBtn : DarkThemeBtn}
-        alt="Toggle Theme"
-        className="w-6 h-6"
+    <label className="flex cursor-pointer gap-2 items-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <circle cx="12" cy="12" r="5" />
+        <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+      </svg>
+
+      <input
+        type="checkbox"
+        checked={isDark}
+        onChange={() => setIsDark(!isDark)}
+        className="toggle theme-controller"
       />
-    </button>
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
+      </svg>
+    </label>
   );
 }
