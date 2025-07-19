32f34679a2b0194b3265f35aa403fcc891f8f111
Roxana Dandu <rosebeats09@gmail.com>
2025-07-19 15:02:35 -0400

    Update style

--- frontend/src/components/allUsers.jsx
+++ frontend/src/components/allUsers.jsx
@@ -28,16 +28,16 @@ export default function AllUsers() {
   return (
     <div className="hide-on-small">
       <p className="text-left text-base-content text-xl font-semibold border-b border-base-300 pb-2 mb-2">You may know</p>
-      <div className=" overflow-y-auto max-h-[80vh]">
-        <ul className="flex flex-col gap-2 justify-start items-start pl-[0px] min-w-[136px] mr-[50px]">
+      <div className="overflow-y-auto max-h-[80vh] pr-2">
+        <ul className="flex flex-col gap-2 justify-start items-start pl-0 min-w-[136px]">
           {allUsers.length > 0
             ? allUsers.map((user) => (
-              <a href={`/users/${user.username}`} className="no-underline text-base-content">
-                <li key={user.id} className="flex gap-[20px] w-[230px]">
+              <a key={user.id} href={`/users/${user.username}`} className="no-underline text-base-content w-full">
+                <li className="flex gap-4 w-full">
                   <img
                     src={user.profile_image}
                     alt="profile image"
-                    className="w-[40px] h-[40px] rounded-full object-cover shrink-0"
+                    className="w-10 h-10 rounded-full object-cover shrink-0"
                   ></img>
                   <p className="truncate pt-[5px]">{user.username}</p>
                 </li>
