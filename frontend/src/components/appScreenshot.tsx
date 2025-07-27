0f62b62a30cee0357b3ef809aa463e703506631f
Roxana Dandu <rosebeats09@gmail.com>
2025-07-27 14:41:23 -0400

    Refactor appScrenshot to tsx

---
+++ frontend/src/components/appScreenshot.tsx
@@ -0,0 +1,8 @@
+const AppScreenshot: React.FC = () => { 
+    return (
+        <div className="w-[800px] h-[600px] bg-base-200 rounded-lg flex items-center justify-center text-base-content text-xl font-semibold">
+            Image Placeholder
+        </div>
+    )
+}
+export default AppScreenshot;
