9cf58af0400a39189e484af26db60620792521bb
Roxana Dandu <rosebeats09@gmail.com>
2025-07-27 14:40:35 -0400

    Refactor about.tsx

---
+++ frontend/src/custom.d.ts
@@ -0,0 +1,19 @@
+declare module "*.png" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.svg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.jpg" {
+  const value: string;
+  export default value;
+}
+
+declare module "*.json" {
+  const value: any;
+  export default value;
+}
