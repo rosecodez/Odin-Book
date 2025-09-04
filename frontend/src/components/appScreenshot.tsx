3d28a594abb5723d05ca7b6dcec21c577416b573
Roxana Dandu <rosebeats09@gmail.com>
2025-09-04 19:50:59 -0400

    Replace image placeholder with screenshot

--- frontend/src/components/appScreenshot.tsx
+++ frontend/src/components/appScreenshot.tsx
@@ -1,8 +1,8 @@
+import homeScreenshot from '../assets/homeScreenshot.png';
+import React from 'react';
 const AppScreenshot: React.FC = () => { 
     return (
-        <div className="w-[800px] h-[600px] bg-base-200 rounded-lg flex items-center justify-center text-base-content text-xl font-semibold">
-            Image Placeholder
-        </div>
+        <img src={homeScreenshot} alt="Image Placeholder" className="w-[800px] h-[600px] bg-base-200 rounded-lg flex items-center justify-center text-base-content text-xl font-semibold"/>
     )
 }
 export default AppScreenshot;
